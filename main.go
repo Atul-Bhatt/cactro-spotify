488d71eef20e20f2947bdb4167ad270861320ac6
Atul Bhatt <atulbhatt61@gmail.com>
2025-05-11 12:22:50 +0530

    secrets in env

--- main.go
+++ main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	clientId := os.Getenv("CLIENT_ID")
+	clientSecret := os.Getenv("CLIENT_SECRET")
+
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
-		c.String(200, "Hello, World!")
+		c.String(200, "Hello, World!, %s, %s", clientId, clientSecret)
 	})
 	r.Run() // defaults to ":8080"
 }
