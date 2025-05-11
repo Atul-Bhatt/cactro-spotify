a6200aade505b87ccdc84d4ad21b670d9df88a1e
Atul Bhatt <atulbhatt61@gmail.com>
2025-05-11 12:35:12 +0530

    generate token

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"bytes"
+	"encoding/base64"
+	"io"
 	"log"
+	"net/url"
 	"os"
+
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -13,12 +19,38 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-	clientId := os.Getenv("CLIENT_ID")
-	clientSecret := os.Getenv("CLIENT_SECRET")
 
 	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.String(200, "Hello, World!, %s, %s", clientId, clientSecret)
-	})
+	r.GET("/", generateToken)
 	r.Run() // defaults to ":8080"
+}
+
+func generateToken(c *gin.Context) {
+	clientID := os.Getenv("CLIENT_ID")
+	clientSecret := os.Getenv("CLIENT_SECRET")
+
+	auth := base64.StdEncoding.EncodeToString([]byte(clientID + ":" + clientSecret))
+
+	data := url.Values{}
+	data.Set("grant_type", "client_credentials")
+
+	req, err := http.NewRequest("POST", "https://accounts.spotify.com/api/token", bytes.NewBufferString(data.Encode()))
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Set("Authorization", "Basic "+auth)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusOK {
+		body, _ := io.ReadAll(resp.Body)
+		os.Stdout.Write(body)
+	} else {
+		log.Fatalf("Request failed with status: %s", resp.Status)
+	}
 }
